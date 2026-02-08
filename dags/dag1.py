c1e54439e290ce89ff358d6ed2eff970b6bb5df2
CCP111 <chicongphan58@gmail.com>
2026-02-08 11:39:40 +0700

    Tune aiflow resources

--- dags/dag1.py
+++ dags/dag1.py
@@ -1,1 +1,18 @@
-print('---------congpc-----')
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="k8s_test",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    task = KubernetesPodOperator(
+        name="hello",
+        task_id="hello",
+        image="python:3.9",
+        cmds=["python", "-c"],
+        arguments=["print('Hello from K8s Executor')"],
+    )
